2743c8cb0c92824215333034573bee771f8a9e4c
g1dra <darko.vucetic7@gmail.com>
2022-05-23 13:07:01 +0200

    redis temp 2

--- src/main/java/com/g1dra/blog/config/RedisConfiguration.java
+++ src/main/java/com/g1dra/blog/config/RedisConfiguration.java
@@ -1,21 +1,37 @@
 package com.g1dra.blog.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.cache.CacheConfig;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RedisConfiguration {
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-         return new JedisConnectionFactory();
+    RedissonClient redisson() throws IOException {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
+    CacheManager cacheManager(RedissonClient redissonClient) {
+        Map<String, CacheConfig> config = new HashMap<String, CacheConfig>();
+
+        // create "testMap" cache with ttl = 24 minutes and maxIdleTime = 12 minutes
+        config.put("testMap", new CacheConfig(24 * 60 * 1000, 12 * 60 * 1000));
+        return new RedissonSpringCacheManager(redissonClient, config);
     }
+
+
 }
